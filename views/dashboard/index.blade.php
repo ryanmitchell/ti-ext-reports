ee36ddea0b15cc6167ba4744ab47295cffab7638
Ryan Mitchell <ryan@thoughtcollective.com>
2020-09-30 11:29:41 +0100

    Use blade for layout

--- views/dashboard/index.blade.php
+++ views/dashboard/index.blade.php
@@ -1,15 +1,11 @@
-<?php 
-	
+@php 
 	[$locationParam, $startDate, $endDate] = $this->getParams();
-	
 	$results = $this->getResults();
-	
-?>
+@endphp
+
 	<div class="row-fluid">
 		
-    <?php
-        if (sizeof($this->getLocations()) > 1){
-	?>
+    @if (sizeof($this->getLocations()) > 1)
 					
 	<div class="list-filter" id="filter-list-filter">
 		
@@ -23,9 +19,9 @@
 						
 						<label for="fld-location">Location</label>
 						<select name="location" class="form-control select2-hidden-accessible" id="fld-location">
-							<?php 
-								foreach ($this->getLocations() as $key=>$location) echo '<option value="'.$key.'"'.($key == $locationParam ? ' selected' : '').'>'.$location.'</option>'; 
-							?>
+							@foreach ($this->getLocations() as $key=>$location)
+							<option value="{{ $key }}" @if ($key == $locationParam) selected @endif>{{ $location }}</option>'; 
+							@endforeach
             			</select>
             			
             		</div>
@@ -40,11 +36,11 @@
 						<div id="datepicker-formfixeddate-fixed-startdate" class="control-datepicker">
                     
 							<div class="input-group">
-								<input type="text" id="datepicker-formfixeddate-date-fixed-startdate" class="form-control" autocomplete="off" value="<?= $startDate->format('d-m-Y'); ?>" data-control="datepicker" data-format="dd-mm-yyyy">
+								<input type="text" id="datepicker-formfixeddate-date-fixed-startdate" class="form-control" autocomplete="off" value="{{ $startDate->format('d-m-Y') }}" data-control="datepicker" data-format="dd-mm-yyyy">
 							    <span class="input-group-prepend" data-original-title="" title="">
 							        <span class="input-group-icon" data-original-title="" title=""><i class="fa fa-calendar-o"></i></span> 
 							    </span>
-							    <input type="hidden" name="start_date" value="<?= $startDate->format('Y-m-d'); ?>" data-datepicker-value="<?= $startDate->format('Y-m-d'); ?>">
+							    <input type="hidden" name="start_date" value="<?= $startDate->format('Y-m-d'); ?>" data-datepicker-value="{{ $startDate->format('Y-m-d') }}">
 							</div>
             			
             			</div>						
@@ -61,11 +57,11 @@
 						<div id="datepicker-formfixeddate-fixed-enddate" class="control-datepicker">
                     
 							<div class="input-group">
-								<input type="text" id="datepicker-formfixeddate-date-fixed-enddate" class="form-control" autocomplete="off" value="<?= $endDate->format('d-m-Y'); ?>" data-control="datepicker" data-format="dd-mm-yyyy">
+								<input type="text" id="datepicker-formfixeddate-date-fixed-enddate" class="form-control" autocomplete="off" value="{{ $endDate->format('d-m-Y') }}" data-control="datepicker" data-format="dd-mm-yyyy">
 							    <span class="input-group-prepend" data-original-title="" title="">
 							        <span class="input-group-icon" data-original-title="" title=""><i class="fa fa-calendar-o"></i></span> 
 							    </span>
-							    <input type="hidden" name="end_date" value="<?= $endDate->format('Y-m-d'); ?>" data-datepicker-value="<?= $endDate->format('Y-m-d'); ?>">
+							    <input type="hidden" name="end_date" value="{{ $endDate->format('Y-m-d') }}" data-datepicker-value="{{ $endDate->format('Y-m-d') }}">
 							</div>
             			
             			</div>						
@@ -77,7 +73,7 @@
 				<div class="col-sm-1">
 					
 					<label>&nbsp;</label>
-					<button type="submit" class="btn btn-primary"><?= lang('lang:thoughtco.kitchendisplay::default.btn_view') ?></button>
+					<button type="submit" class="btn btn-primary">@lang('thoughtco.kitchendisplay::default.btn_view')</button>
 					
 				</div>
         
@@ -86,10 +82,7 @@
 		</form>
 		
 	</div>
-	
-    <?php 
-        }
-    ?>	 
+	@endif	 
         
 	<div class="row mx-1">
 			
@@ -97,7 +90,7 @@
 			<div class="card bg-light p-3 shadow-sm">
 				<div class="card-counter sale">
 					<i class="stat-icon  bg-success text-white fa fa-line-chart"></i>
-					<span class="stat-number"><?= $results->total_sales; ?></span>
+					<span class="stat-number">{{ $results->total_sales }}</span>
 					<span class="stat-text">Total Sales</span>
 				</div>
 			</div>
@@ -107,7 +100,7 @@
 			<div class="card bg-light p-3 shadow-sm">
 				<div class="card-counter sale">
 					<i class="stat-icon  bg-success text-white fa fa-line-chart"></i>
-					<span class="stat-number"><?= $results->total_orders; ?></span>
+					<span class="stat-number">{{ $results->total_orders }}</span>
 					<span class="stat-text">Total Orders</span>
 				</div>
 			</div>
@@ -117,7 +110,7 @@
 			<div class="card bg-light p-3 shadow-sm">
 				<div class="card-counter sale">
 					<i class="stat-icon  bg-success text-white fa fa-line-chart"></i>
-					<span class="stat-number"><?= $results->quantity_of_items; ?></span>
+					<span class="stat-number">{{ $results->quantity_of_items }}</span>
 					<span class="stat-text">Total Items</span>
 				</div>
 			</div>
@@ -129,22 +122,26 @@
 		
 		<div class="col col-sm-6">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Top Customers</div>
+				<div class="card-title">
+					<h1 class="h4">Top customers</h1>
+				</div>				
 				<div class="card-body">
-				<?php foreach ($results->top_customers as $item){ ?>
-					<dd><?= $item->name ?> (<?= currency_format($item->value) ?>)</dd>
-				<?php } ?>
+				@foreach ($results->top_customers as $item)
+					<dd>{{ $item->name }} ({{ currency_format($item->value) }})</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
 		
 		<div class="col col-sm-6">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Bottom Customers</div>
+				<div class="card-title">
+					<h1 class="h4">Bottom customers</h1>
+				</div>					
 				<div class="card-body">
-				<?php foreach ($results->bottom_customers as $item){ ?>
-					<dd><?= $item->name ?> (<?= currency_format($item->value) ?>)</dd>
-				<?php } ?>
+				@foreach ($results->bottom_customers as $item)
+					<dd>{{ $item->name }} ({{ currency_format($item->value) }})</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
@@ -155,22 +152,26 @@
 
 		<div class="col col-sm-6">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Top Items</div>
+				<div class="card-title">
+					<h1 class="h4">Top items</h1>
+				</div>					
 				<div class="card-body">
-				<?php foreach ($results->top_items as $item){ ?>
-					<dd><?= $item->name ?> (<?= $item->quantity ?>)</dd>
-				<?php } ?>
+				@foreach ($results->top_items as $item)
+					<dd>{{ $item->name }} ({{ $item->quantity }})</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
 		
 		<div class="col col-sm-6">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Bottom Items</div>
+				<div class="card-title">
+					<h1 class="h4">Bottom items</h1>
+				</div>				
 				<div class="card-body">
-				<?php foreach ($results->bottom_items as $item){ ?>
-					<dd><?= $item->name ?> (<?= $item->quantity ?>)</dd>
-				<?php } ?>
+				@foreach ($results->bottom_items as $item)
+					<dd>{{ $item->name }} ({{ $item->quantity }})</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
@@ -181,33 +182,39 @@
 				
 		<div class="col col-sm-4">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Orders by day</div>
-				<div class="card-body">
-				<?php foreach ($results->orders_by_day as $key=>$item){ ?>
-					<dd><?= date('l', strtotime("Sunday + $key Days")); ?> <?= currency_format($item->value) ?> / <?= $item->count ?> </dd>
-				<?php } ?>
+			<div class="card-title">
+				<h1 class="h4">Orders by day</h1>
+			</div>
+			<div class="card-body">
+				@foreach ($results->orders_by_day as $key=>$item)
+					<dd>{{ date('l', strtotime("Sunday + $key Days")) }} {{ currency_format($item->value) }} / {{ $item->count }}</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
 
 		<div class="col col-sm-4">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Orders by hour</div>
+				<div class="card-title">
+					<h1 class="h4">Orders by hour</h1>
+				</div>
 				<div class="card-body">
-				<?php foreach ($results->orders_by_hour as $key=>$item){ ?>
-					<dd><?= str_pad($key, 2, '0', STR_PAD_LEFT).':00 - '.str_pad($key+1, 2, '0', STR_PAD_LEFT).':00' ?> <?= currency_format($item->value) ?> / <?= $item->count ?> </dd>
-				<?php } ?>
+				@foreach ($results->orders_by_hour as $key=>$item)
+					<dd>{{ str_pad($key, 2, '0', STR_PAD_LEFT).':00 - '.str_pad($key+1, 2, '0', STR_PAD_LEFT).':00' }} {{ currency_format($item->value) }} / {{ $item->count }}</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
 
 		<div class="col col-sm-4">
 			<div class="card bg-light p-3 shadow-sm">
-				<div class="card-title">Orders by category</div>
+				<div class="card-title">
+					<h1 class="h4">Orders by category</h1>
+				</div>
 				<div class="card-body">
-				<?php foreach ($results->orders_by_category as $key=>$item){ ?>
-					<dd><?= $item->name ?> <?= currency_format($item->value) ?> / <?= $item->count ?> </dd>
-				<?php } ?>
+				@foreach ($results->orders_by_category as $key=>$item)
+					<dd>{{ $item->name }} {{ currency_format($item->value) }} / {{ $item->count }}</dd>
+				@endforeach
 				</div>
 			</div>
 		</div>
